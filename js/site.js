9ec6bee0acf2dd4e0f7f197f13eeaea0dae93ce8
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2021-10-20 18:01:02 -0500

    basic css

---
+++ js/site.js
@@ -0,0 +1,8 @@
+function incheck() {
+  firstName = document.getElementById("fName").value;
+  lastName = document.getElementById("lName").value;
+  email = document.getElementById("emailId").value;
+  phone = document.getElementById("phoneNum").value;
+  dob = document.getElementById("BirthDate").value;
+  
+	
