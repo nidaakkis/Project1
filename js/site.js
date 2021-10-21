b4c58c92f97b6e88c08661c1f3d21880bd43fd61
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2021-10-20 19:15:20 -0500

    code in some js

--- js/site.js
+++ js/site.js
@@ -1,8 +1,36 @@
-function incheck() {
-  firstName = document.getElementById("fName").value;
-  lastName = document.getElementById("lName").value;
-  email = document.getElementById("emailId").value;
-  phone = document.getElementById("phoneNum").value;
-  dob = document.getElementById("BirthDate").value;
+function init() {
+  document.getElementById("submit").addEventListener("click", function(evnt) {
+	evnt.preventDefault();
+	alert("Thanks for Subscribing! Happy Learning!");
+	});
+};
+  function incheck() {
+    firstName = document.getElementById("fName").value;
+    lastName = document.getElementById("lName").value;
+    email = document.getElementById("emailId").value;
+    phone = document.getElementById("phoneNum").value;
+    dob = document.getElementById("BirthDate").value;
+  
+    if(!firstName || !lastName || !email || !phone || !dob) {
+      alert("There are missing fields! Please fill all in all fields");
+	  return false;
+    }
+}
+
+function chckbox() {
+  if(!document.getElementById("age-verification").checked) {
+	alert("Please check that you are 18 or older");
+	return false;
+  }
+  if(!document.getElementById("crime-verification").checked) {
+	alert("Please check that you won't commit any of these crimes");
+	return false;
+  }
+  else {
+	return true;
+  }
+}
+
+//window.addEventListener('load', init);
   
 	
